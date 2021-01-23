e4975fef1a2f29bc845fb5e634c9780c47f467a9
Manoj Awasthi <awasthi.manoj@gmail.com>
2021-01-23 15:12:16 +0700

    adding type information to prime

--- logic/isPrime.hs
+++ logic/isPrime.hs
@@ -1,10 +1,13 @@
+ld :: Integer -> Integer
 ld n = ldf 2 n
 
+ldf :: Integer -> Integer -> Integer
 ldf k n 
     | n `mod` k == 0 = k
     | k^2 > n = n
     | otherwise = ldf (k+1) n
 
+isPrime :: Integer -> Bool
 isPrime n 
     | n < 1 = error "not a positive integer"
     | n == 1 = False
