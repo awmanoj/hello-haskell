6585e4c3795982e6bfcf935548923d765475515f
Manoj Awasthi <awasthi.manoj@gmail.com>
2021-02-01 23:45:00 +0700

    Logical validations

--- logic/TAMO.hs
+++ logic/TAMO.hs
@@ -8,3 +8,13 @@ x <==> y = x == y
 
 (<+>) :: Bool -> Bool -> Bool
 x <+> y = x /= y
+
+formula2 p q = ((not p) && (p ==> q) <==> not (q && (not p)))
+
+valid1 bf = (bf False) && (bf True)
+valid2 bf = (bf False False) 
+         && (bf False True)
+         && (bf True False) 
+         && (bf True True)
+
+valid3 bf = and [bf p q r | p <- [True, False], q <- [True, False], r <- [True, False]]
